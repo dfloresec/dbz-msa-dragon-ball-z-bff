d24ff30c3061df06bbaf24310192164042fdf836
Diego Flores <diego.flores.guerra@gmail.com>
2024-03-13 12:13:42 -0500

    Inclusión de cabeceras

--- src/main/java/ec/com/dragonballz/repository/FavoritesUpdateClient.java
+++ src/main/java/ec/com/dragonballz/repository/FavoritesUpdateClient.java
@@ -3,11 +3,13 @@ package ec.com.dragonballz.repository;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import ec.com.dragonballz.service.dto.RequestDto;
+import feign.Headers;
 import feign.RequestLine;
 
 public interface FavoritesUpdateClient {
 
 	@RequestLine("POST /favorite")
+	@Headers("Content-Type: application/json")
 	public void changeStatus(@RequestBody RequestDto req);
 
 }
